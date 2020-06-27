00b9cd2094b1741b36fe94bae388b0f69a67826e
David White <dwhite0627@gmail.com>
2020-06-26 21:25:12 -0400

    Attempt partial stretch of the array-based names duplicate function

--- names/names.py
+++ names/names.py
@@ -25,19 +25,53 @@ duplicates = []  # Return the list of duplicates in this data structure
 
 # A more efficient approach using binary search trees
 # Add all names in names_1 to a binary search tree
-bst = BSTNode("a")
+# bst = BSTNode("a")
 
-for name in names_1:  # O(n)
-    # ipdb.set_trace()
-    bst.insert(name)  # O(nlogn)
-# This runs in the worst case of O(nlogn)
+# for name in names_1:  # O(n)
+#     # ipdb.set_trace()
+#     bst.insert(name)  # O(nlogn)
+# # This runs in the worst case of O(nlogn)
 
-for name in names_2:  # O(n)
-    if bst.contains(name):  # O(nlogn)
-        duplicates.append(name)  # O(1)
+# for name in names_2:  # O(n)
+#     if bst.contains(name):  # O(nlogn)
+#         duplicates.append(name)  # O(1)
 # This also runs in the worst case of O(nlogn)
 # --------------------------------------------------
 # 2O(nlogn) reduces to O(nlogn)
+
+
+## Stretch ##
+
+# Using sorting and index comparison to find repeated values in the sorted list
+# time complexity: O(n log n)
+# runtime: 0.011981964111328125 seconds
+names = names_1 + names_2  # O(2n)
+names.sort()  # O(nlogn)
+counter = 1  # O(1)
+repeat = ""  # O(1)
+for idx in range(len(names)):  # O(n + k)
+    ipdb.set_trace()
+    current_name = names[idx]  # O(1)
+    if idx > 0:  # O(1)
+        if names[idx - 1] == names[idx]:  # O(1)
+            counter += 1  # O(1)
+            repeat = current_name  # O(1)
+            if counter >= 2:  # O(1) and prevents multiple duplicates of same name
+                duplicates.append(names[idx])  # O(1)
+        else:
+            # Reset counter, both at O(1)
+            counter = 1
+            repeat = ""
+
+# # uncomment if there can be more than 2 of each name in the combined list
+# duplicates = set(duplicates)  # Adds another O(n) pass
+# duplicates = list(duplicates)  # Adds another O(n) pass
+# ------------------------------------------------------------------------
+# For the data as is, the above is O(nlogn) + O(2n) * (3 * O(1)) = O(nlogn)
+# For the data with more duplicates, O(logn)
+
+
+## End Stretch ##
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
