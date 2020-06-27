32865a0285de57176cec567e684b83b123d47448
David White <dwhite0627@gmail.com>
2020-06-26 20:47:48 -0400

    Add RingBuffer class that passes all tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,41 @@
+#import ipdb
+
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.oldest = 0
+        self.buffer = []
 
     def append(self, item):
-        pass
+        # ipdb.set_trace()
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(item)
+        else:
+            self.buffer[self.oldest] = item
+        self.oldest = (self.oldest + 1) % self.capacity
 
     def get(self):
-        pass
+        return [node for node in self.buffer if node is not None]
+
+
+# Manual testing
+# buffer = RingBuffer(3)
+
+# print(buffer.get())   # should return []
+
+# buffer.append('a')
+# buffer.append('b')
+# buffer.append('c')
+
+# print(buffer.get())   # should return ['a', 'b', 'c']
+
+# # 'd' overwrites the oldest value in the ring buffer, which is 'a'
+# buffer.append('d')
+
+# print(buffer.get())   # should return ['d', 'b', 'c']
+
+# buffer.append('e')
+# buffer.append('f')
+
+# print(buffer.get())   # should return ['d', 'e', 'f']
