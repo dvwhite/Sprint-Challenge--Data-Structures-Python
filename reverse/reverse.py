0960f02df87a3a01dfb40fb988a8d430dc634703
David White <dwhite0627@gmail.com>
2020-06-26 21:58:33 -0400

    Add partially completed reverse function

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,6 +12,7 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -39,4 +40,56 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        nodes = []
+        current = node
+        while node is not None:
+            next = node.next_node
+            nodes.append(node)
+            node.next_node = None
+            node = next
+
+        print([node.value for node in nodes])
+
+        for node in nodes:
+            print(node.value)
+            next = self.head.next_node
+            self.head = node
+            self.head.next_node = next
+
+        #print([node.value for node in nodes])
+
+        # if prev is None:
+        #     nodes.append(node)
+        # else:
+        #     if prev.prev:
+        #         nodes.append(prev.reverse_list(prev, prev.prev))
+        # return nodes
+
+    def __str__(self):
+        values = ""
+        node = self.head
+        while node is not None:
+            values += f'({node.value})'
+            if node.get_next():
+                values += '->'
+            node = node.get_next()
+        return values
+
+
+ll = LinkedList()
+ll.add_to_head(3)
+ll.add_to_head(2)
+ll.add_to_head(1)
+print(ll)
+last = ""
+ll.reverse_list(ll.head, ll.head)
+print(ll)
+# node = ll.head
+# while node is not None:
+#     if node.get_next():
+#         node = node.get_next()
+#     else:
+#         last = node
+#         node = None
+# print(last.value) # 3
+# ll.reverse(last)
